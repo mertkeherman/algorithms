4d856c97ac7dd6b1d5c7dca9aa33a6ea9c0924f3
mertkeherman <93578153+mertkeherman@users.noreply.github.com>
2021-11-23 08:00:07 +0100

    Create ClimbingStairs.js

---
+++ ClimbingStairs.js
@@ -0,0 +1,13 @@
+const climbStairs = (n) => {
+    if(n <= 3) return n;
+    
+    const ways = [0,1,2,3]
+    
+    for(let i = 4; i <= n; i++){
+        ways.push(ways[i-1] + ways[i-2]);
+    }
+    
+    return ways[n];
+};
+
+console.log(climbStairs(4));
